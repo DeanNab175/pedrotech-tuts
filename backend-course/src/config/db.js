a291b17696c2eed3146c1ede25aeb95e5e1c2e1d
satyajit.nabeebacus <satyajit.nabeebacus@rapp.com>
2026-05-04 14:32:10 +0400

    add backend course initial file and db setup

---
+++ backend-course/src/config/db.js
@@ -0,0 +1,30 @@
+import "dotenv/config";
+import { PrismaClient } from "../../generated/prisma/client.ts";
+import { PrismaPg } from "@prisma/adapter-pg";
+
+const connectionString = `${process.env.DATABASE_URL}`;
+const adapter = new PrismaPg({ connectionString });
+
+const prisma = new PrismaClient({
+  adapter,
+  log:
+    process.env.NODE_ENV === "development"
+      ? ["query", "error", "warn"]
+      : ["error"],
+});
+
+async function connectDB() {
+  try {
+    await prisma.$connect();
+    console.log("DB connected via Prisma");
+  } catch (error) {
+    console.error(`Database connection error: ${error.message}`);
+    process.exit(1);
+  }
+}
+
+async function disconnectDB() {
+  await prisma.$disconnect();
+}
+
+export { prisma, connectDB, disconnectDB };
---
+++ backend-course/src/server.js
@@ -0,0 +1,45 @@
+import express from "express";
+import { config } from "dotenv";
+
+import { connectDB, disconnectDB } from "./config/db.js";
+
+// Import Routes
+import movieRoutes from "./routes/movieRoutes.js";
+
+config();
+connectDB();
+
+const app = express();
+const PORT = 5001;
+
+// API Routes
+app.use("/movies", movieRoutes);
+
+const server = app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
+
+// Handle unhandled promise rejections (e.g. database connection errors)
+process.on("unhandledRejection", (err) => {
+  console.error("Unhandled Rejection: ", err);
+  server.close(async () => {
+    await disconnectDB();
+    process.exit(1);
+  });
+});
+
+// Handle uncaught exceptions
+process.on("uncaughtException", async (err) => {
+  console.error("Uncaught Exception: ", err);
+  await disconnectDB();
+  process.exit(1);
+});
+
+// Graceful shutdown
+process.on("SIGTERM", (err) => {
+  console.log("SIGTERM received shutting down gracefully: ");
+  server.close(async () => {
+    await disconnectDB();
+    process.exit(0);
+  });
+});
